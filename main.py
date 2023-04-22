5fb3bef3302d9bd8290a0d438b77e0be3b1bcb2f
u11kumar <47977758+u11kumar@users.noreply.github.com>
2023-04-22 16:33:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter.messagebox
 from tkinter import *
 from turtle import width
 
@@ -10,7 +11,7 @@ window=Tk()
 window.geometry("600x250")
 window.configure(bg=col1)
 window.resizable(width=False, height=False)
-
+#function for converting values to hexadecimal and showing in the text box
 def scale(value):
     r = int(red_scale.get())
     g = int(green_scale.get())
@@ -22,8 +23,14 @@ def scale(value):
     screen["bg"]=hexadecimal
     color_entry.delete(0,END)
     color_entry.insert(0,hexadecimal)
-
-
+#function for copying 
+def oneclick():
+    tkinter.messagebox.showinfo('Color',"Copied Successfully")
+    clip = Tk()
+    clip.withdraw()
+    clip.clipboard_clear()
+    clip.clipboard_append(color_entry.get())
+    clip.destroy()
 
 screen=Label(window,bg=col0,width=40,height=10)
 screen.grid(row=0,column=0)
@@ -51,14 +58,14 @@ blue.grid(row=2,column=0)
 
 blue_scale=Scale(right_frame,command=scale,bg=col1,fg="blue",from_=0,to=255,length=150,orient=HORIZONTAL)
 blue_scale.grid(row=2,column=1)
-
+#textbox of hexcode and copy code button
 rgb_label= Label(down_frame,bg=col1,text="HEX CODE :",fg="black",anchor=NW,font=("Ivy",10,"bold"))
 rgb_label.grid(row=0,column=0,pady=5)
 
 color_entry=Entry(down_frame,width=12,font=("Ivy",10,"bold"),justify=CENTER)
 color_entry.grid(row=0,column=1,padx=5)
 
-copy_button= Button(down_frame,text="Copy Color Code",bg=col1,font=("Ivy",10,"bold"))
+copy_button= Button(down_frame,text="Copy Color Code",bg=col1,font=("Ivy",10,"bold"),command=oneclick)
 copy_button.grid(row=0,column=2,padx=5)
 
 app_name=Label(down_frame,text="Color Picker App",bg=col1,font=("Ivy",20,"bold"))
